4e3c6bed48ceee84400d0442d3409ec107abdc0b
Salonee-Jain <salonijain1248@gmail.com>
2021-08-17 19:49:42 +0530

    added changes to JS file

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
 
- 
+//display mobile menu
+
+const MobileMenu = () =>{
+    menu.classList.toggle("is-active")
+    menuLinks.classList.toggle("active")
+}
+
+menu.addEventListener('click', MobileMenu)
