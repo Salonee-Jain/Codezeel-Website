514d2ed4572d6296e8c37c86512e4519ba8365e2
Salonee-Jain <salonijain1248@gmail.com>
2021-08-21 01:54:23 +0530

    final code

--- app.js
+++ app.js
@@ -1,27 +1,55 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
-const navLogo= document.querySelector('#navbar__logo');
+const navLogo = document.querySelector('#navbar__logo');
 
-//display mobile menu
+// Display Mobile Menu
+const mobileMenu = () => {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+};
 
-const MobileMenu = () =>{
-    menu.classList.toggle("is-active");
-    menuLinks.classList.toggle("active");
-}
+menu.addEventListener('click', mobileMenu);
 
-menu.addEventListener('click', MobileMenu);
+// Show active menu when scrolling
+const highlightMenu = () => {
+  const elem = document.querySelector('.highlight');
+  const homeMenu = document.querySelector('#home-page');
+  const aboutMenu = document.querySelector('#about-page');
+  const servicesMenu = document.querySelector('#services-page');
+  let scrollPos = window.scrollY;
+  // console.log(scrollPos);
 
+  // adds 'highlight' class to my menu items
+  if (window.innerWidth > 960 && scrollPos < 600) {
+    homeMenu.classList.add('highlight');
+    aboutMenu.classList.remove('highlight');
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 1400) {
+    aboutMenu.classList.add('highlight');
+    homeMenu.classList.remove('highlight');
+    servicesMenu.classList.remove('highlight');
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 2345) {
+    servicesMenu.classList.add('highlight');
+    aboutMenu.classList.remove('highlight');
+    return;
+  }
 
-// show active menu 
-const highlightMenu =  () =>{
-    const element = document.querySelector('.highlight');
-    const homeMenu = document.querySelector('.#home-page');
-    const aboutMenu = document.querySelector('#about-page');
-    const servicesPage = document.querySelector('#services-page');
-    let scrollPos = window.scrollY;
+  if ((elem && window.innerWIdth < 960 && scrollPos < 600) || elem) {
+    elem.classList.remove('highlight');
+  }
+};
 
+window.addEventListener('scroll', highlightMenu);
+window.addEventListener('click', highlightMenu);
 
-    i
-
-
-}
+const hideMobileMenu = () => {
+    const menuBars = document.querySelector('.is-active');
+    if (window.innerWidth <= 768 && menuBars) {
+      menu.classList.toggle('is-active');
+      menuLinks.classList.remove('active');
+    }
+  };
+  
+  menuLinks.addEventListener('click', hideMobileMenu);
+  navLogo.addEventListener('click', hideMobileMenu);
