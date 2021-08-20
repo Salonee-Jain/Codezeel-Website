84d48747a2881ac5f444a25a9bf6b2c103839ed3
Salonee-Jain <salonijain1248@gmail.com>
2021-08-21 01:30:37 +0530

    Added styling to social media icons

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar__menu')
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+const navLogo= document.querySelector('#navbar__logo');
 
 //display mobile menu
 
 const MobileMenu = () =>{
-    menu.classList.toggle("is-active")
-    menuLinks.classList.toggle("active")
+    menu.classList.toggle("is-active");
+    menuLinks.classList.toggle("active");
 }
 
 menu.addEventListener('click', MobileMenu);
 
+
+// show active menu 
+const highlightMenu =  () =>{
+    const element = document.querySelector('.highlight');
+    const homeMenu = document.querySelector('.#home-page');
+    const aboutMenu = document.querySelector('#about-page');
+    const servicesPage = document.querySelector('#services-page');
+    let scrollPos = window.scrollY;
+
+
+    i
+
+
+}
